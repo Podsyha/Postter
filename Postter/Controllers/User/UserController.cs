bbba94c37c25b96267186ff96ad990921ec96184
Александр Подсухин <podsyha@gmail.com>
2022-10-02 13:23:42 +0500

    набросмок usecase аккаунта

--- Postter/Controllers/User/UserController.cs
+++ Postter/Controllers/User/UserController.cs
@@ -8,7 +8,6 @@ namespace Postter.Controllers.User;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
-
     public UserController(AppDbContext dbContext)
     {
         _dbContext = dbContext;
@@ -17,9 +16,60 @@ public class UserController : ControllerBase
     private readonly AppDbContext _dbContext;
     
     
-    [HttpGet]
-    public Task<Infrastructure.DAO.User> Get(Guid id)
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginModel model)
     {
-        return _dbContext.User.FirstAsync(x => x.Id == id);
+        User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+        if (user != null)
+        {
+            await Authenticate(model.Email); // аутентификация
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View(model);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(RegisterModel model)
+    {
+        User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+        if (user == null)
+        {
+            // добавляем пользователя в бд
+            db.Users.Add(new User { Email = model.Email, Password = model.Password });
+            await db.SaveChangesAsync();
+
+            await Authenticate(model.Email); // аутентификация
+
+            return RedirectToAction("Index", "Home");
+        }
+        else
+            ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+
+        return View(model);
+    }
+
+    private async Task Authenticate(string userName)
+    {
+        // создаем один claim
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+        };
+        // создаем объект ClaimsIdentity
+        ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType,
+            ClaimsIdentity.DefaultRoleClaimType);
+        // установка аутентификационных куки
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return RedirectToAction("Login", "Account");
     }
 }
---
+++ Postter/UseCases/UseCaseAccount.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Postter.Common.Exceptions;
+using Postter.Controllers.User.Model;
+using Postter.Infrastructure.Context;
+using Postter.Infrastructure.DAO;
+
+namespace Postter.UseCases;
+
+public class UseCaseAccount
+{
+    public UseCaseAccount(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    private readonly AppDbContext _dbContext;
+
+
+    
+    public async Task Login(LoginModel model)
+    {
+        User user = await _dbContext.User.FirstOrDefaultAsync(u =>
+            u.Email == model.Email && u.Password == model.Password);
+        if (user != null)
+            Authenticate(model.Email);
+        else
+            throw new RequestLogicException("Некорректные логин и(или) пароль");
+    }
+
+    public async Task Register(RegisterModel model)
+    {
+        User user = await _dbContext.User.FirstOrDefaultAsync(u => u.Email == model.Email);
+        if (user == null)
+        {
+            _dbContext.User.Add(new User { Email = model.Email, Password = model.Password });
+            await _dbContext.SaveChangesAsync();
+
+            Authenticate(model.Email);
+        }
+        else
+            throw new RequestLogicException("Некорректные логин и(или) пароль");
+    }
+
+    private ClaimsIdentity Authenticate(string userName)
+    {
+        var claims = new List<Claim>
+        {
+            new (ClaimsIdentity.DefaultNameClaimType, userName)
+        };
+
+        ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType,
+            ClaimsIdentity.DefaultRoleClaimType);
+
+        return id;
+    }
+}
