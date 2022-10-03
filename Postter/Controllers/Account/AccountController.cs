90be5e9546a6dfb208b4b34562a114ad96274349
Александр Подсухин <podsyha@gmail.com>
2022-10-03 19:25:13 +0500

    summary

--- Postter/Controllers/Account/AccountController.cs
+++ Postter/Controllers/Account/AccountController.cs
@@ -21,15 +21,20 @@ public class AccountController : ControllerBase
 
     
     
+    /// <summary>
+    /// Получить токен аутентификации
+    /// </summary>
+    /// <param name="email">Почта</param>
+    /// <param name="password">Пароль</param>
     [HttpGet("/token")]
     [AllowAnonymous]
-    public async Task<IActionResult> Token(string username, string password)
+    public async Task<IActionResult> Token(string email, string password)
     {
-        ClaimsIdentity identity = await _useCaseAccount.GetIdentity(username, password);
+        ClaimsIdentity identity = await _useCaseAccount.GetIdentity(email, password);
         if (identity == null)
             return BadRequest("Неверная почта или пароль.");
 
-        JwtSecurityToken token = _useCaseAccount.GetToken(username, password, identity);
+        JwtSecurityToken token = _useCaseAccount.GetToken(email, password, identity);
         
         string encodedJwt = new JwtSecurityTokenHandler().WriteToken(token);
 
@@ -44,6 +49,13 @@ public class AccountController : ControllerBase
         return Ok(response);
     }
 
+    /// <summary>
+    /// Зарегистрировать нового пользователя
+    /// </summary>
+    /// <param name="email">Почта</param>
+    /// <param name="password">Пароль</param>
+    [HttpPost("/GiveAdminRole")]
+    [AllowAnonymous]
     public async Task<IActionResult> Register(string email, string password)
     {
         await _useCaseAccount.Register(email, password);
@@ -53,6 +65,11 @@ public class AccountController : ControllerBase
         return Ok();
     }
 
+    /// <summary>
+    /// Выдать роль пользователю
+    /// </summary>
+    /// <param name="email">Почта пользователя</param>
+    /// <param name="role">необходимая роль</param>
     [HttpPost("/GiveAdminRole")]
     [Authorize(Roles = "admin")]
     public async Task<IActionResult> GiveTheUserARole(string email, string role)
