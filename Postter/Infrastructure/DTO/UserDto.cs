6774098e7d3bfb5778108e473c49bd80602972a2
Александр Подсухин <podsyha@gmail.com>
2022-10-06 14:40:42 +0500

    Почистил проект от старых моделей

--- Postter/Infrastructure/DTO/UserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Postter.Infrastructure.DTO;
-
-public class UserDto
-{
-    public Guid Id { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-}
